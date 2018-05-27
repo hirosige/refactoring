7f797450c9f3d41b8512182863530aa54f4f8cae
Hiroshige.Negishi <hirosige1@gmail.com>
2018-05-27 07:19:53 +0700

    initial commit 2

--- chapter1/Main.java
+++ chapter1/Main.java
@@ -4,9 +4,17 @@ import movies.Customer;
 
 class Main {
     public static void main(String[] args) {
-        Customer hiro = new Customer("hiro");
-        String result = hiro.statement();
+        Movie mib = new Movie("Men in Black", 0);
+        Rental rentMib = new Rental(mib, 5);
 
+        Movie mib2 = new Movie("Men in Black2", 1);
+        Rental rentMib2 = new Rental(mib2, 5);
+
+        Customer hiro = new Customer("hiro");
+        hiro.addRental(rentMib);
+        hiro.addRental(rentMib2);
+
+        String result = hiro.statement();
         System.out.println(result);
     }
 }
--- chapter1/movies/Customer.java
+++ chapter1/movies/Customer.java
@@ -7,11 +7,11 @@ public class Customer {
     private String _name;
     private Vector _rentals = new Vector();
 
-    public Customer (String name) {
+    public Customer(String name) {
         _name = name;
     }
 
-    public void addRental (Rental arg) {
+    public void addRental(Rental arg) {
         _rentals.addElement(arg);
     }
 
